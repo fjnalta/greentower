f8e91c2e0c8a8f6e8bbe112005f0b156df18d0af
Max Blatt <max.blatt@web.de>
2016-03-23 20:03:44 +0100

    Removed unnecessary code.

--- GreenTower/core/src/com/greentower/states/PlayState.java
+++ GreenTower/core/src/com/greentower/states/PlayState.java
@@ -8,7 +8,6 @@ import com.badlogic.gdx.Input.Keys;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.maps.MapObject;
 import com.badlogic.gdx.maps.objects.RectangleMapObject;
 import com.badlogic.gdx.maps.tiled.TiledMap;
@@ -19,64 +18,71 @@ import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.math.Vector3;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
-import com.badlogic.gdx.physics.box2d.Box2DDebugRenderer;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 import com.badlogic.gdx.physics.box2d.PolygonShape;
 import com.badlogic.gdx.physics.box2d.World;
 import com.greentower.GreenTowerGame;
 import com.greentower.sprites.Player;
 
+
+/**
+ * The {@link State} that contains the main game logic.
+ */
 public class PlayState extends State {
 	
-	private static final int TILE_WIDTH = 64;
-	private static final int TILE_HEIGHT = 64;
 	public static final int TILES_HORIZONTAL = 20;
 	
 	private Player player;
-	//private Texture bg;
 	
-	//TODO: For testing purposes
-	private SpriteBatch batch;
 	private BitmapFont font;
-	private OrthographicCamera cam;
+	private OrthographicCamera camera;
 		
-	
-	private TmxMapLoader maploader;
-	private OrthogonalTiledMapRenderer renderer;
 	private TiledMap map;
+	private TmxMapLoader maploader;
+	
+	private OrthogonalTiledMapRenderer mapRenderer;
 	
 	private List<Rectangle> listrect = new ArrayList<Rectangle>();
 	
 	//Box2d variables for physics and colliders
 	private World world;
-	private Box2DDebugRenderer b2dr;
 
-	protected PlayState(GameStateManager gsm) {
-		super(gsm);	
-		//create player
-		player = new Player(64,64);
-		//set background texture
-		//bg = new Texture("bg.png");
-		//only need one camera -> derive from state
+	
+	/**
+	 * Initializes a new instance of the {@link PlayState} class.
+	 * 
+	 * @param gamestateManager the {@link GameStateManager} of the state.
+	 */
+	protected PlayState(GameStateManager gamestateManager) {
+		super(gamestateManager);	
+		
+		player = new Player(64, 64);
 
 		font = new BitmapFont();
 		
-		//create the Map
-		createMap();
+		createMap("SciFiStage.tmx");
 		
-		cam = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
-		cam.position.set( GreenTowerGame.WIDTH / 2, GreenTowerGame.HEIGHT/2,0);
-		cam.update();
+		camera = new OrthographicCamera(Gdx.graphics.getWidth(), Gdx.graphics.getHeight());
+		camera.position.set(
+				GreenTowerGame.WIDTH / 2,
+				GreenTowerGame.HEIGHT/2,
+				0);
+		
+		camera.update();
 	}
 	
-	private void createMap(){
+	/**
+	 * Creates the main map.
+	 * 
+	 * @param mapFileName the name of the tmx file that contains the map data.
+	 */
+	private void createMap(String mapFileName) {
 		maploader = new TmxMapLoader();
 		//load bottom map
-		map = maploader.load("SciFiStage.tmx");
-		renderer = new OrthogonalTiledMapRenderer(map);
+		map = maploader.load(mapFileName);
+		mapRenderer = new OrthogonalTiledMapRenderer(map);
 		
 		world = new World(new Vector2(0, 0), true);
-		b2dr = new Box2DDebugRenderer();
 		
 		BodyDef bdef = new BodyDef();
 		PolygonShape shape = new PolygonShape();
@@ -103,9 +109,36 @@ public class PlayState extends State {
 		}		
 	}
 
+	
+	
 	/**
-	 * Handling all the Player inputs here
+	 * Updates the game logic.
 	 * 
+	 * @param delta the time since the last update call.
+	 */
+	@Override
+	public void update(float dt) {
+		//always handle the input first
+		handleInput();
+		
+		world.step(1/60f, 6, 2);
+		//playerCollision();
+		//then everything else
+		player.update(dt);
+		//update the camera position relative to the player
+		
+		if(player.getPosition().y > GreenTowerGame.HEIGHT/2 ){
+			//camPosYbefore = player.getPosition().y;
+			camera.position.set( GreenTowerGame.WIDTH / 2,player.getPosition().y ,0 );
+		}
+
+		//update the camera
+		mapRenderer.setView(camera);
+		camera.update();
+	}
+	
+	/**
+	 * Handling all the Player inputs here.
 	 */
 	@Override
 	protected void handleInput() {
@@ -113,7 +146,8 @@ public class PlayState extends State {
 		{
 			player.jump();
 		}
-		if(Gdx.input.isKeyPressed(Keys.LEFT) || Gdx.input.isKeyPressed(Keys.RIGHT)){
+		if(Gdx.input.isKeyPressed(Keys.LEFT) || Gdx.input.isKeyPressed(Keys.RIGHT))
+		{
 			if(Gdx.input.isKeyPressed(Keys.LEFT))
 			{
 				player.setMoveDirection(new Vector3(-1,0,0));
@@ -127,116 +161,42 @@ public class PlayState extends State {
 		}
 		
 	}
-	
-//	protected void playerCollision() {
-//		Tile[] tiles = tilemap.getSurroundingTiles(player.getPosition().x, player.getPosition().y, 4);
-//		for(Tile tile : tiles) {
-//			player.
-//		}
-//	}
-	
-	
-	float camPosYbefore = 0;
-	/**
-	 * Updates the game logic.
-	 * 
-	 * @param delta the time since the last update call.
-	 */
-	@Override
-	public void update(float dt) {
-		//always handle the input first
-		handleInput();
-		//then collision
-		world.step(1/60f, 6, 2);
-		//playerCollision();
-		//then everything else
-		player.update(dt);
-		//update the camera position relative to the player
-		
-		if(player.getPosition().y > GreenTowerGame.HEIGHT/2 ){
-			//camPosYbefore = player.getPosition().y;
-			cam.position.set( GreenTowerGame.WIDTH / 2,player.getPosition().y ,0 );
-		}
-		//TODO - create new platforms here
-		
-
-		//update the camera
-		renderer.setView(cam);
-		cam.update();
-	}
 
 	
 	/**
-	 * Packages everything into a Spritebatch and renders it to the screen
+	 * Packages everything into a {@link SpriteBatch} and renders it to the
+	 * screen.
 	 * 
-	 * @param sb Spritebatch
+	 * @param spritebatch the {@link SpriteBatch} used for drawing.
 	 */
 	@Override
-	public void render(SpriteBatch sb) {
-		sb.setProjectionMatrix(cam.combined);
+	public void render(SpriteBatch spritebatch) {
+		spritebatch.setProjectionMatrix(camera.combined);
+		
 		//draw tileMap
-		renderer.render();
+		mapRenderer.render();
 		//start packaging
-		sb.begin();
-		//draw background in the middle of the screen
-	//	sb.draw(bg, cam.position.x - cam.viewportWidth / 2, 0);
+		
+		spritebatch.begin();
+		
 		//draw the player
-		sb.draw(player.getTexture(), player.getPosition().x, player.getPosition().y);
+		spritebatch.draw(player.getTexture(), player.getPosition().x, player.getPosition().y);
 		//TODO: For testing purposes
-		font.draw(sb, ""+player.state, player.getPosition().x+2, player.getPosition().y+20);
-		font.draw(sb, "VelX: "+(int)player.getVelocity().x, player.getPosition().x+2, player.getPosition().y+40);
-		font.draw(sb, "VelY: "+(int)player.getVelocity().y, player.getPosition().x+2, player.getPosition().y+60);
+		font.draw(spritebatch, ""+player.state, player.getPosition().x+2, player.getPosition().y+20);
+		font.draw(spritebatch, "VelX: "+(int)player.getVelocity().x, player.getPosition().x+2, player.getPosition().y+40);
+		font.draw(spritebatch, "VelY: "+(int)player.getVelocity().y, player.getPosition().x+2, player.getPosition().y+60);
 		
-		//TODO - create Map
-		
-		ShapeRenderer sh = new ShapeRenderer();
-		sh.setAutoShapeType(true);
-		sh.begin();
-		for(Rectangle rec : listrect){
-			sh.rect(rec.x,rec.y,rec.width,rec.height  );
-		}
-		sh.end();
-		
-		
-		/*for(Platform platform : platforms)
-		{
-			spritebatch.draw(
-					imgPlatform,
-					platform.getRect().x,
-					platform.getRect().y,
-					platform.getRect().width,
-					platform.getRect().height);
-			
-		}*/
-		
-//		for(int y = 0; y < tilemap.getHeight(); y++)
-//		{
-//			Tile[] row = tilemap.getRow(y);
-//			
-//			for(int x = 0; x < tilemap.getWidth(); x++)
-//			{
-//				Tile tile = row[x];
-//				if(tile != null)
-//				{
-//					Int32Point2D worldPos = tileCamera.tileToWorld(x, y);
-//					Int32Point2D screenPos = tileCamera.screenToWorld(worldPos.x, worldPos.y);
-//				
-//					sb.draw(
-//					tile.texture,
-//					screenPos.x,
-//					screenPos.y,
-//					TILE_WIDTH,
-//					TILE_HEIGHT);
-//				}
-//			}
-//		}
-		
-		sb.end();
+		spritebatch.end();
 	}
 
+	/**
+	 * Frees unmanaged resources.
+	 */
 	@Override
 	public void dispose() {
-		// TODO Auto-generated method stub
+		mapRenderer.dispose();
+		font.dispose();
+		map.dispose();
 	}
 
 }
